ba395949e4986973756467098f0472c7522b8b3d
unknown <davidhu52497@gmail.com>
2022-04-19 21:14:54 -0400

    about and navbar

---
+++ frontend_react/src/container/About/About.jsx
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react'
+import {motion} from 'framer-motion';
+import {images} from '../../constants'
+
+import './About.scss';
+const abouts = [
+  { title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01},
+  { title: 'Front end', description: 'I am proficient with React.', imgUrl: images.about02},
+  { title: 'Back end', description: 'I am proficient with Node.js.', imgUrl: images.about03},
+  { title: 'Full stack', description: 'I am a good full stack engineer.', imgUrl: images.about04},
+]
+
+const About = () => {
+  return (
+      <>
+      <h2 className = "head-text"> I know That <span>Good Apps</span><br/> means <span>Good Business</span>
+      </h2>
+      <div className = "app__profiles">
+         {abouts.map((about,index) => (
+           <motion.div
+           whileInView = {{opacity: 1}}
+           whileHover= {{scale: 1.1}}
+           transition = {{duration: 0.5, type: 'tween'}}
+           className = "app__profile-item"
+           key = {about.title + index}
+           >
+           <img src = {about.imgUrl} alt = {about.title} />
+           <h2 className = "bold-text" style = {{marginTop: 20}}> {about.title}</h2>
+           <p className = "p-text" style = {{marginTop: 10}}> {about.description}</p>
+           </motion.div>
+         ))}
+      </div>
+      </>
+  )
+}
+
+export default About
